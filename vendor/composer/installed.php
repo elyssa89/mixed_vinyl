3b086947460b6b8b4928ab550eb53bc4758188c0
elyssa89 <safaelyssa89@gmail.com>
2023-11-15 08:10:30 +0100

    Add initial set of files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,50 @@
+<?php return array(
+    'root' => array(
+        'name' => 'symfony/skeleton',
+        'pretty_version' => 'v5.4.99',
+        'version' => '5.4.99.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'symfony/flex' => array(
+            'pretty_version' => 'v1.21.2',
+            'version' => '1.21.2.0',
+            'reference' => 'f1cf4014ffac79e0c031cc40d6d66354e7de5da5',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../symfony/flex',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/polyfill-ctype' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'symfony/polyfill-iconv' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'symfony/polyfill-php72' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'symfony/skeleton' => array(
+            'pretty_version' => 'v5.4.99',
+            'version' => '5.4.99.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
